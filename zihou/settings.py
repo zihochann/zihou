9e16878b51dafcfdccdb37dbf96c801c31f1e956
Saki <tomguts@126.com>
2021-02-27 16:35:24 +0800

    fixed static root.

--- zihou/settings.py
+++ zihou/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
